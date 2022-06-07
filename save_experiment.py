9e263a00dd4da19596d9cedd86b4a852de24d7f4
Ye Min Aung <redyaung2003@gmail.com>
2022-06-07 13:01:14 -0400

    Add naming functionality to save_experiments.py
    Naming experiments is a core feature that should have been implemented
    since the inception of this script. This eliminates the extra step of
    renaming every experiment after generating the directory with this
    script.

--- save_experiment.py
+++ save_experiment.py
@@ -8,6 +8,9 @@ The capture directories are copied instead of moved to facilitate further
 experiments with the same capture. The calibration_result.json file is
 also copied to keep the most recent results for inspection purposes. The
 remaining visualization files are simply moved into the experiment folder.
+
+We can extend this by storing more data related to the experiment inside
+the directory in a systematic way.
 """
 
 import os
@@ -15,9 +18,19 @@ import os.path
 import glob
 import datetime
 import shutil
+import argparse
 
 
 def main():
+    # Accept an optional argument describing the name of the experiment.
+    parser = argparse.ArgumentParser(
+        description="Save data relating to the experiment into a directory",
+        formatter_class=argparse.RawTextHelpFormatter
+    )
+    parser.add_argument("-name", type=str, default=None)
+    args = parser.parse_args()
+    name = args.name
+
     # Find all the capture directories.
     captures = sorted(glob.glob(".\\capture_*"))
     assert len(captures) > 0, "No capture directories were found."
@@ -32,7 +45,8 @@ def main():
 
     # Create the new experiment directory.
     now = datetime.datetime.now()
-    experiment_dir = ".\\experiments\\e_" + now.strftime("%Y%m%d_%H%M%S")
+    experiment_dir = ".\\experiments\\e_" + now.strftime("%Y%m%d_%H%M%S") \
+        if name is None else f".\\experiments\\{name}"
     os.mkdir(experiment_dir)
 
     # Store the items in the experiment directory.
